a9a591e6bae06db771858e51b07ddde8427f6780
siyoung-j <tldud49@naver.com>
2019-06-10 10:31:17 +0900

    190610 수정

--- ZETA.Plugin/src/com/zero/zeta/plugin/Zenkins.java
+++ ZETA.Plugin/src/com/zero/zeta/plugin/Zenkins.java
@@ -1,10 +1,24 @@
 package com.zero.zeta.plugin;
 
+import java.util.Calendar;
+
 public class Zenkins {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("젠킨스 연동 완료");
+		
+		Calendar c = Calendar.getInstance();
+
+		String nowTime = String.format("%d-%02d-%02d %02d:%02d:%02d"
+										, c.get(Calendar.YEAR)
+										, c.get(Calendar.MONTH) + 1
+										, c.get(Calendar.DAY_OF_MONTH)
+										, c.get(Calendar.HOUR_OF_DAY)
+										, c.get(Calendar.MINUTE)
+										, c.get(Calendar.SECOND));
+		
+		System.out.println("현재 시각 : " + nowTime);
 		
 	}
 
